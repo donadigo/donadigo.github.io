3fd8edcae0f16b968961298d8f29ce06f9ff09a5
Adam Bieńkowski <donadigos159@gmail.com>
2020-02-17 19:38:09 +0100

    Add a new page

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const IFRAME_ASPECT_RATIO = 1.77;
+
 function toggleDark() {
     if (document.body.classList.contains("body-dark")) {
         document.body.classList.remove("body-dark");
@@ -10,8 +12,21 @@ function toggleDark() {
     }
 }
 
+
 const darkToggleButton = document.getElementById("dark-toggle-btn");
 darkToggleButton.onclick = toggleDark;
 if (document.cookie == "dark") {
     toggleDark();
 }
+
+function resizeFrames() {
+    const frames = document.querySelectorAll("iframe");
+    for (const frame of frames) {
+        const rect = frame.parentElement.getBoundingClientRect();
+        frame.style.width = rect.width.toString() + "px";
+        frame.style.height = (rect.width / IFRAME_ASPECT_RATIO).toString() + "px";
+    }
+}
+
+window.addEventListener("resize", resizeFrames);
+resizeFrames();
